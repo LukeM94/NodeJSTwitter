f66ba526452b5fbb1ae8623b7c930a791e5ff27c
LukeM94 <DRU9HEde>
2015-12-17 00:48:50 +0000

    Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luke
+ * Date: 17/12/2015
+ * Time: 00:46
+ */
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<body>
+
+<h1>Fuck off you scumbag!</h1>
+
+</body>
